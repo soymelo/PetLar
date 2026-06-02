92996e94b0333ca37ea0502f0b5bce3daa61ef05
Luan Gabriel Melo da Rocha <luan.rocha@pfmconsultoria.com.br>
2026-06-02 15:27:50 -0300

    feat: repository interfaces.

---
+++ PetLar.Core/Repositories/IPetRepository.cs
@@ -0,0 +1,12 @@
+﻿using PetLar.Core.Entities;
+
+namespace PetLar.Core.Repositories;
+
+public interface IPetRepository
+{
+    Task AddAsync(Pet pet);
+    Task<Pet?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Pet>> GetAllPetsAsync();
+    Task UpdateAsync(Pet pet);
+    Task<IEnumerable<Pet>> GetByOwnerIdAsync(Guid ownerId);
+}
---
+++ PetLar.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using PetLar.Core.Entities;
+
+namespace PetLar.Core.Repositories;
+
+public interface IUserRepository
+{
+    Task AddAsync(User user);
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User?> GetByEmailAsync(string email);
+}
