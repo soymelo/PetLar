fbadfbb45bfde1a16c1025cb3537b020426d04b3
Luan Gabriel Melo da Rocha <luan.rocha@pfmconsultoria.com.br>
2026-06-09 14:55:36 -0300

    feat(application): implement GetPetById query and handler.

---
+++ PetLar.Application/Pets/Handlers/GetPetByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using PetLar.Application.Pets.Queries;
+using PetLar.Application.Pets.ViewModels;
+using PetLar.Core.Entities;
+using PetLar.Core.Interfaces;
+
+namespace PetLar.Application.Pets.Handlers;
+
+public class GetPetByIdQueryHandler(IPetRepository _petRepository) : IRequestHandler<GetPetByIdQuery, PetViewModel?>
+{
+    public async Task<PetViewModel?> Handle(GetPetByIdQuery request, CancellationToken ct)
+    {
+        var pet = await _petRepository.GetByIdAsync(request.Id);
+        if (pet is null)
+            return null;
+
+        return new PetViewModel(
+            pet.Id,
+            pet.Name,
+            pet.Age,
+            pet.Gender,
+            pet.Species,
+            pet.Size,
+            pet.Status,
+            pet.OwnerId
+        );
+    }
+}
---
+++ PetLar.Application/Pets/Queries/GetPetByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using PetLar.Application.Pets.ViewModels;
+
+namespace PetLar.Application.Pets.Queries;
+
+public record GetPetByIdQuery(
+    Guid Id
+) : IRequest<PetViewModel?>;
