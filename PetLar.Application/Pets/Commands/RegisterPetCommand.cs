3fe287e3e939599e0ca03ef886e49366b38e5b15
Luan Gabriel Melo da Rocha <luan.rocha@pfmconsultoria.com.br>
2026-06-09 14:37:47 -0300

    feat(application): implement RegisterPet command and handler.

---
+++ PetLar.Application/Pets/Commands/RegisterPetCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using PetLar.Core.Enums;
+
+namespace PetLar.Application.Pets.Commands;
+
+public record RegisterPetCommand(
+    string Name,
+    int Age,
+    EnumGender Gender,
+    EnumSpecies Species,
+    EnumSize Size,
+    Guid OwnerId
+) : IRequest<Guid>;
---
+++ PetLar.Application/Pets/Handlers/RegisterPetCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using PetLar.Application.Pets.Commands;
+using PetLar.Application.Users.Commands;
+using PetLar.Core.Entities;
+using PetLar.Core.Enums;
+using PetLar.Core.Interfaces;
+
+namespace PetLar.Application.Pets.Handlers;
+
+public class RegisterPetCommandHandler(IPetRepository _petRepository) : IRequestHandler<RegisterPetCommand, Guid>
+{
+    public async Task<Guid> Handle(RegisterPetCommand request, CancellationToken ct)
+    {
+        var pet = new Pet
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Age = request.Age,
+            Gender = request.Gender,
+            Species = request.Species,
+            Size = request.Size,
+            Status = EnumPetStatus.Available,
+            OwnerId = request.OwnerId,
+        };
+
+        await _petRepository.AddAsync(pet);
+
+        return pet.Id;
+    }
+}
