672b92835c0f77e75d1aac965654131b62a4f21b
Luan Gabriel Melo da Rocha <luan.rocha@pfmconsultoria.com.br>
2026-07-07 11:53:11 -0300

    feat(application): implement GetPetsByOwnerId query and handler.

---
+++ PetLar.Application/Pets/Handlers/GetPetsByOwnerIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using PetLar.Application.Pets.DTOs;
+using PetLar.Application.Pets.Queries;
+using PetLar.Core.Interfaces;
+
+namespace PetLar.Application.Pets.Handlers;
+
+public class GetPetsByOwnerIdQueryHandler(IPetRepository _petRepository) : IRequestHandler<GetPetsByOwnerIdQuery, IEnumerable<PetDto>>
+{
+    public async Task<IEnumerable<PetDto>> Handle(GetPetsByOwnerIdQuery request, CancellationToken ct)
+    {
+        var pets = await _petRepository.GetByOwnerIdAsync(request.OwnerId);
+
+        var petsDto = pets.Select(p => new PetDto(
+            p.Id,
+            p.Name,
+            p.Age,
+            p.Gender,
+            p.Species,
+            p.Size,
+            p.Status,
+            p.OwnerId
+        )).ToList();
+
+        return petsDto;
+    }
+}
---
+++ PetLar.Application/Pets/Queries/GetPetsByOwnerIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using PetLar.Application.Pets.DTOs;
+
+namespace PetLar.Application.Pets.Queries;
+
+public record GetPetsByOwnerIdQuery(
+    Guid OwnerId
+) : IRequest<IEnumerable<PetDto>>;
