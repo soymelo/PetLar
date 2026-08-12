1734492d952d58bc8d1b4f259e9a5dd0f3b746c0
Luan Gabriel Melo da Rocha <luan.rocha@pfmconsultoria.com.br>
2026-08-12 14:48:54 -0300

    feat: translate pet attributes for display.

---
+++ PetLar.Web/Extensions/EnumDisplayExtensions.cs
@@ -0,0 +1,31 @@
+﻿using PetLar.Core.Enums;
+
+namespace PetLar.Web.Extensions;
+
+public static class EnumDisplayExtensions
+{
+    public static string ToDisplayName(this PetGender gender) =>
+    gender switch
+    {
+        PetGender.Male => "Macho",
+        PetGender.Female => "Fêmea",
+        _ => gender.ToString()
+    };
+
+    public static string ToDisplayName(this PetSpecies species) =>
+    species switch
+    {
+        PetSpecies.Dog => "Cachorro",
+        PetSpecies.Cat => "Gato",
+        _ => species.ToString()
+    };
+
+    public static string ToDisplayName(this PetSize size) =>
+    size switch
+    {
+        PetSize.Small => "Pequeno",
+        PetSize.Medium => "Médio",
+        PetSize.Large => "Grande",
+        _ => size.ToString()
+    };
+}
